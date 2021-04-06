bca92dbda90ce61833fba513a5417ababe210c3d
peanut <jjun4341@naver.com>
2021-04-06 13:28:33 +0900

    feat: Setting Window - Set Background Color

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 root = Tk()
 
 root.geometry('480x360')
+root.title("Schedule Manager")
 
 # Label, Entry 등 먼저 정의하고 나중에 한꺼번에 .pack() 하기
 # 용어 정의
@@ -80,13 +81,51 @@ def Add_Card():
     doneb = Button(a, padx=10, pady=5, text = '완료', command=addcard)
     doneb.pack()
 
+def config():
+    a = Toplevel(root)
+    a.geometry('480x360')
+    CheckVar1 = IntVar()
+
+    CheckVar2 = IntVar()
+
+    c1 = Checkbutton(a, text="다크 모드", var=CheckVar1)
+
+    c2 = Checkbutton(a, text="화이트 모드", var=CheckVar2)
+
+    c1.pack()
+    c2.pack()
+
+    def change_bgcolor():
+        if CheckVar1.get() == 1:
+
+            root['bg'] = '#000000'
+
+        elif CheckVar2.get() == 1:
+
+            root['bg'] = '#ffffff'
+
+        else:
+            pass
+
+
+    donb = Button(a, padx=10, pady=5, text='완료', command=change_bgcolor)
+    donb.pack()
+
+
+
 ################# 함수 정의 끝 #################
 
-add_kanban = Button(root, padx=10, pady=5, text='열(Column) 추가', command=Add_Column)
-add_kanban.pack()
-
-add_card = Button(root, padx=10, pady=5, text='카드(Card) 추가', command=Add_Card)
+add_kanban = Button(root, height=2, width=15, text='열(Column) 추가', command=Add_Column)
+add_card = Button(root, height=2, width=15, text='카드 (Card) 추가', command=Add_Card)
+see_cc = Button(root, height=2, width=15, text='열 & 카드 보기')
+setting_b = Button(root, height=2, width=15, text='설정', command=config)
 
 add_card.pack()
+add_kanban.pack()
+
+add_kanban.place(x=30, y=50)
+add_card.place(x=30, y=100)
+see_cc.place(x=30, y=150)
+setting_b.place(x=30, y=200)
 
 root.mainloop()
